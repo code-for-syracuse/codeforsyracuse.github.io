5a8f4471378777796d0b99df343a341fe2e1fc7a
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-08-01 14:00:37 -0400

    added react

---
+++ components/button.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import "../App.css"
+
+class Button extends React.Component {
+    render() {
+      return <button className="styles.orangeButton"></button>;
+    }
+  }
